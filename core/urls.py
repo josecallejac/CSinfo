7082d68989f66d1c9cdbbf9ef19046476c007246
josecallejac <jose.ue98@gmail.com>
2023-06-05 14:48:40 -0400

    hltv namespace

--- core/urls.py
+++ core/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', HomeView.as_view(), name="home"),
-    path('hltv/', include('hltv.urls')),
+    path('hltv/', include('hltv.urls', namespace="hltv")),
     path('api/', include('api.urls')),
     path('docs/', include_docs_urls(title='Api CSinfo Documentation'))
     
