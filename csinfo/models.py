1a5f852a1c423e5a35284332c8b4dab963f61e03
josecallejac <jose.ue98@gmail.com>
2023-06-12 21:19:04 -0400

    model Torneo

--- csinfo/models.py
+++ csinfo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Torneo(models.Model):
+    nombreTorneo = models.CharField(max_length=100)
+    locacionTorneo = models.CharField(max_length=100)
+    teamsParticipando = models.IntegerField()
+    premio = models.IntegerField()
+    fechaInicio = models.DateField(verbose_name='fecha_inicio' )
+    fechaTermino = models.DateField(verbose_name='fecha_termino')
+    topPlayer = models.CharField(max_length=100)
+    teamCampeon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombreTorneo
